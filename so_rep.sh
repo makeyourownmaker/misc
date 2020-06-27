a03b4768e0e4c36726f59e1660653fd080fdfb14
Derek Wilson <derekw@derekw.org>
2020-06-27 16:47:55 +0100

    Added negative reputation variable to account for downvotes and closed questions.  TODO Check why jq is not treating negative reputation correctly

--- so_rep.sh
+++ so_rep.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Get stackoverflow reputation and update beeminder goal if it has increased
-# WARNING: Watch out for problems with downvoting on stackoverflow
+# WARNING Watch out for problems with downvoting etc on stackoverflow
+# TODO Replace NEG_REP kludge with jq fix
+#      Why jq not handling negative numbers properly?
 
 set -euo pipefail
 
@@ -9,10 +11,15 @@ PATH=/usr/local/bin:/usr/bin:/bin:~/bin
 
 SOID=<stackoverflow ID>
 GOAL=<beeminder goal>
+NEG_REP=0 # downvotes (-1) and closed questions (-10)
 
-# WARNING: Got to be careful with SO rep
-#          Downvoting costs rep points
-so_rep_full=`curl -s "https://api.stackexchange.com/2.2/users/$SOID/reputation-history?pagesize=100&site=stackoverflow" | gunzip | jq '[.items[].reputation_change] | add'`
+# NOTE Can only retreive reputation history in 100 unit pages without authorisation
+#      See https://api.stackexchange.com/docs
+# WARNING Got to be careful with SO rep
+#          Downvoting costs rep points (-1)
+#          Answering closed questions costs rep points! (-10)
+so_rep_1=`curl -s "https://api.stackexchange.com/2.2/users/$SOID/reputation-history?pagesize=100&site=stackoverflow&page=1" | gunzip | jq '[.items[].reputation_change] | add'`
+so_rep_full=$((so_rep_1-NEG_REP))
 #echo $so_rep_full # DEBUG
 #exit
 
